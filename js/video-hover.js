b173e62488128175fdbcf7d94b4bc9d582e43dbb
johnchan215 <johnnychan1996@gmail.com>
2023-11-20 19:13:22 +0800

    Change mouse event to pointer event

--- js/video-hover.js
+++ js/video-hover.js
@@ -2,12 +2,12 @@ const video = document.querySelectorAll("video");
 const cards = document.querySelectorAll(".projects");
 
 cards.forEach((card, i) => {
-    card.addEventListener('mouseenter', () => {
+    card.addEventListener('pointerenter', () => {
       video[i].play();
       video[i].playbackRate = 2;
     });
     
-    card.addEventListener('mouseleave', () => {
+    card.addEventListener('pointerleave', () => {
       video[i].pause();
       video[i].currentTime = 0;
     });
