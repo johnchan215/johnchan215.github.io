92b83fc3950d1a6c8bbe404b1b830b43c99bd2f8
johnchan215 <johnnychan1996@gmail.com>
2023-11-24 22:44:35 +0800

    Remove video time reset

--- js/video-hover.js
+++ js/video-hover.js
@@ -1,18 +1,23 @@
-const video = document.querySelectorAll("video");
+const videos = document.querySelectorAll("video");
 const cards = document.querySelectorAll(".projects");
 
 cards.forEach((card, i) => {
-  ['pointerenter','touchmove'].forEach((e) => {
+
+  ['pointerenter', 'touchmove'].forEach((e) => {
     card.addEventListener(e, () => {
-      video[i].play();
-      video[i].playbackRate = 2;
+      videos[i].play();
+      videos[i].playbackRate = 2;
     });
   });
-    
-  ['pointerleave','touchend'].forEach((e) => {
+
+  ['pointerleave'].forEach((e) => {
     card.addEventListener(e, () => {
-      video[i].pause();
-      video[i].currentTime = 0;
+      videos.forEach(video => {
+        if (!video.paused) {
+          video.pause();
+        }
+      });
     });
   });
-  });
+
+});
