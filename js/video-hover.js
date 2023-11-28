715931b95e349512a571fc283c97db69c91ec80c
johnchan215 <johnnychan1996@gmail.com>
2023-11-29 01:21:39 +0800

    Fix video hover for desktop and mobile

--- js/video-hover.js
+++ js/video-hover.js
@@ -3,21 +3,38 @@ const cards = document.querySelectorAll(".projects");
 
 cards.forEach((card, i) => {
 
+  // Play video 2x speed
+  videos[i].playbackRate = 2;
+
+  // Play video when desktop or mobile hover
   ['pointerenter', 'touchmove'].forEach((e) => {
     card.addEventListener(e, () => {
       videos[i].play();
-      videos[i].playbackRate = 2;
     });
   });
 
-  ['pointerleave'].forEach((e) => {
-    card.addEventListener(e, () => {
+  // Code to be executed on mobile devices
+  if (/Mobi|Android/i.test(navigator.userAgent)) {
+    // Stop video when mobile leaves card element
+    card.addEventListener('pointerleave', (evt) => {
       videos.forEach(video => {
-        if (!video.paused) {
+        if (!video.paused && evt.target.querySelector('video') == video) {
+          return;
+        } else {
           video.pause();
+          video.currentTime = 0;
         }
       });
     });
-  });
+    // Code to be executed on non-mobile devices
+  } else {
+    // Stop video when desktop leaves card element
+    card.addEventListener('mouseleave', () => {
+      videos.forEach(video => {
+        video.pause();
+        video.currentTime = 0;
+      });
+    });
+  }
 
 });
