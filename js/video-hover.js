109d9e3c1f4f60def14aee68025836c39861b877
johnchan215 <johnnychan1996@gmail.com>
2024-01-31 15:54:18 +1100

    Video hover only for desktop

--- js/video-hover.js
+++ js/video-hover.js
@@ -1,41 +1,24 @@
-const videos = document.querySelectorAll("video");
 const cards = document.querySelectorAll(".projects");
+const videos = document.querySelectorAll("video");
 
 cards.forEach((card, i) => {
 
+  // Pause video after autoplay for video poster
   videos[i].pause();
   // Play video 2x speed
   videos[i].playbackRate = 2;
 
-  // Play video when desktop or mobile hover
-  ['pointerenter', 'touchmove'].forEach((e) => {
-    card.addEventListener(e, () => {
-      videos[i].play();
-    });
+  // Play video when mouse enters video
+  card.addEventListener('mouseenter', () => {
+    videos[i].play();
   });
 
-  // Code to be executed on mobile devices
-  if (/Mobi|Android/i.test(navigator.userAgent)) {
-    // Stop video when mobile leaves card element
-    card.addEventListener('pointerleave', (evt) => {
-      videos.forEach(video => {
-        if (!video.paused && evt.target.querySelector('video') == video) {
-          return;
-        } else {
-          video.pause();
-          video.currentTime = 0;
-        }
-      });
+  // Stop and reset video when mouse leaves video
+  card.addEventListener('mouseleave', () => {
+    videos.forEach(video => {
+      video.pause();
+      video.currentTime = 0;
     });
-    // Code to be executed on non-mobile devices
-  } else {
-    // Stop video when desktop leaves card element
-    card.addEventListener('mouseleave', () => {
-      videos.forEach(video => {
-        video.pause();
-        video.currentTime = 0;
-      });
-    });
-  }
+  });
 
 });
