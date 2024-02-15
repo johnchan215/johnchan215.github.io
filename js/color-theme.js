ef5a14afa559f3aaaf81cce9a81acfb995c837a7
johnchan215 <johnnychan1996@gmail.com>
2024-02-16 00:30:36 +1100

    Format with Prettier

--- js/color-theme.js
+++ js/color-theme.js
@@ -1,23 +1,23 @@
 function setTheme(theme) {
-    document.documentElement.setAttribute('data-bs-theme', theme);
-    if (theme === 'dark') {
-        document.getElementById('color-icon').className = "bi bi-moon-stars-fill";
-    } else {
-        document.getElementById('color-icon').className = "bi bi-sun-fill";
-    }
+  document.documentElement.setAttribute("data-bs-theme", theme);
+  if (theme === "dark") {
+    document.getElementById("color-icon").className = "bi bi-moon-stars-fill";
+  } else {
+    document.getElementById("color-icon").className = "bi bi-sun-fill";
+  }
 }
 
 function toggleTheme() {
-    if (theme === 'dark') {
-        theme = 'light';
-    } else {
-        theme = 'dark';
-    }
-    setTheme(theme);
-    localStorage.setItem("theme", theme);
+  if (theme === "dark") {
+    theme = "light";
+  } else {
+    theme = "dark";
+  }
+  setTheme(theme);
+  localStorage.setItem("theme", theme);
 }
 
 let theme = localStorage.getItem("theme");
 
-document.getElementById('color-toggle').addEventListener('click', toggleTheme);
+document.getElementById("color-toggle").addEventListener("click", toggleTheme);
 setTheme(theme);
--- js/video-hover.js
+++ js/video-hover.js
@@ -2,23 +2,21 @@ const cards = document.querySelectorAll(".projects");
 const videos = document.querySelectorAll("video");
 
 cards.forEach((card, i) => {
-
   // Pause video after autoplay for video poster
   videos[i].pause();
   // Play video 2x speed
   videos[i].playbackRate = 2;
 
   // Play video when mouse enters video
-  card.addEventListener('mouseenter', () => {
+  card.addEventListener("mouseenter", () => {
     videos[i].play();
   });
 
   // Stop and reset video when mouse leaves video
-  card.addEventListener('mouseleave', () => {
-    videos.forEach(video => {
+  card.addEventListener("mouseleave", () => {
+    videos.forEach((video) => {
       video.pause();
       video.currentTime = 0;
     });
   });
-
 });
